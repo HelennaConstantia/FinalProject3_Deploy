fe7b47f2fa57cfc6ed74576d27f6500db7b00f49
HelennaConstantia <constantiahelenna9@gmail.com>
2021-12-16 14:32:35 +0700

    requirements

--- app.py
+++ app.py
@@ -9,10 +9,17 @@ def form():
     return render_template("main.html")
 @app.route('/predict',methods=["POST"])
 def predict():
-    int_features=[float(x) for x in request.form.values()]
+    age=float(request.form['age'])
+    creatin=float(request.form['creatin'])
+    ejection=float(request.form['ejection'])
+    platelets=float(request.form['platelets'])
+    serum_c=float(request.form['serum_c'])
+    serum_s=float(request.form['serum_s'])
+    time=float(request.form['time'])
+    int_features=[age,creatin,ejection,platelets,serum_c,serum_s,time]
     final_features=[np.array(int_features)]
     prediction=model.predict(final_features)
-    output={0:'survive', 1:'not survive'}
+    output={0:'tidak meninggal', 1:'meninggal'}
     return render_template('main.html', prediction_text='Pasien penderita penyakit jantung {}'.format(output[prediction[0]]))
 if __name__=='__main__':
     app.run(debug=True)
